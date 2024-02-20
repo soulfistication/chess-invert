a58623c99ea37cbff4872d5dfc918f48bad9ffdf
Ivan Almada <ivan.almada@me.com>
2024-02-20 11:31:14 -0600

    8x8 checkered board with first row of IBOutlets

--- chess-invert/ViewController.m
+++ chess-invert/ViewController.m
@@ -7,6 +7,23 @@
 
 #import "ViewController.h"
 
+@import CocoaAsyncSocket;
+
+@interface ViewController ()
+
+#pragma mark - IBOutlets
+
+@property (weak) IBOutlet NSView *view11;
+@property (weak) IBOutlet NSView *view12;
+@property (weak) IBOutlet NSView *view13;
+@property (weak) IBOutlet NSView *view14;
+@property (weak) IBOutlet NSView *view15;
+@property (weak) IBOutlet NSView *view16;
+@property (weak) IBOutlet NSView *view17;
+@property (weak) IBOutlet NSView *view18;
+
+@end
+
 @implementation ViewController
 
 #pragma mark - UIViewController
@@ -17,6 +34,21 @@
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
+}
+
+#pragma mark - Helpers
+
+- (void)setupViews {
+    [self setupFirstRow];
+}
+
+- (void)setupFirstRow {
+
+}
+
+- (void)setupWhiteView:(NSView *)view {
+    view.wantsLayer = YES;
+    view.layer.backgroundColor = [NSColor whiteColor].CGColor;
 }
 
 @end
