dcc033f8a4a7d35a5703d483e8ce54834dcf31a5
Ivan Almada <ivan.almada@me.com>
2024-02-20 10:56:58 -0600

    Basic Cocoa App

--- chess-invert/AppDelegate.m
+++ chess-invert/AppDelegate.m
@@ -7,26 +7,20 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
-
-
-@end
-
 @implementation AppDelegate
+
+#pragma mark - App Delegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
 }
 
-
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
-
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
     return YES;
 }
-
 
 @end
--- chess-invert/ViewController.m
+++ chess-invert/ViewController.m
@@ -13,14 +13,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
 }
-
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-    // Update the view, if already loaded.
 }
 
 @end
